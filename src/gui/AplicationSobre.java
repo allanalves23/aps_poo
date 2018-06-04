24a6eb7d05c1bcbe36d15009e4242d7bfb646f25
Allan Wanderley Alves <allanalves23@outlook.com>
2018-06-03 22:53:03 -0300

    Adicionado tela sobre e remocao JOptionPane na funcao

---
+++ src/gui/AplicationSobre.java
@@ -0,0 +1,116 @@
+package gui;
+
+/**
+ *
+ * @author allan wanderley alves
+ * aps programação orientada a objeto - 2018.1
+ * ultima modificação 03/06/2018
+ */
+public class AplicationSobre extends javax.swing.JDialog {
+//Tela (JDialog) 'About' do sistema ao acessar o button 'ajuda' 
+   
+    public AplicationSobre(java.awt.Frame parent, boolean modal,String version) {
+        super(parent, modal);
+        initComponents();
+        this.version.setText(version);
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        lblVersion = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        version = new javax.swing.JLabel();
+        lblMsg2 = new javax.swing.JLabel();
+        lblMsg1 = new javax.swing.JLabel();
+
+        jLabel1.setText("jLabel1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("About");
+        setAlwaysOnTop(true);
+        setMaximumSize(new java.awt.Dimension(128, 128));
+        setMinimumSize(new java.awt.Dimension(128, 128));
+        setResizable(false);
+        setSize(new java.awt.Dimension(128, 128));
+        setType(java.awt.Window.Type.UTILITY);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        lblVersion.setText("Versão");
+
+        jLabel2.setText("Desenvolvido por Allan Wanderley Alves");
+
+        lblMsg2.setText("Orientada a Objetos");
+
+        lblMsg1.setText("Atividade Prática Supervisionada de Programação");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblMsg1)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(72, 72, 72)
+                        .addComponent(lblMsg2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(jLabel2))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(lblVersion)
+                        .addGap(38, 38, 38)
+                        .addComponent(version, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(53, 53, 53)))
+                .addGap(20, 20, 20))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(70, 70, 70)
+                .addComponent(lblMsg1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblMsg2)
+                .addGap(27, 27, 27)
+                .addComponent(jLabel2)
+                .addGap(57, 57, 57)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(version, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblVersion))
+                .addContainerGap(137, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+   
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblMsg1;
+    private javax.swing.JLabel lblMsg2;
+    private javax.swing.JLabel lblVersion;
+    private javax.swing.JLabel version;
+    // End of variables declaration//GEN-END:variables
+}
